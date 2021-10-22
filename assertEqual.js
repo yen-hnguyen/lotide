bdf62f68a9f91e6876970116527c97edf641a7bd
Yen <yen.hnguyen17@gmail.com>
2021-10-22 00:42:11 +0000

    function to test our code

---
+++ assertEqual.js
@@ -0,0 +1,13 @@
+// Function Implementation
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`❌❌❌ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`✅✅✅ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+//Test Code
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Yen", "Yen");

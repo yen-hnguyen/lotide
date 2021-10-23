8b42cbae144651a51afd1ed1608b7a31f6a90492
Yen <yen.hnguyen17@gmail.com>
2021-10-23 04:40:04 +0000

    Adding equal arrays function to compare 2 arrays

---
+++ eqArrays.js
@@ -0,0 +1,28 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = (array1, array2) => {
+  if (array1.length !== array2.length) {
+    return false;
+  } else {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        return false;
+      } else {
+        return true;
+      }
+    }
+  }
+};
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true)
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true)
+assertEqual(eqArrays(["Yen", "Jackie"], ["Yen", "Jackie"]), false)
+
+
+

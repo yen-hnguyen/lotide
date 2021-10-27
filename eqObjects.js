f08d24bbd2140d6255884c13ac2123720d39edee
Yen <yen.hnguyen17@gmail.com>
2021-10-27 05:26:32 +0000

    Refractor the function to remove unnecessary lines

--- eqObjects.js
+++ eqObjects.js
@@ -34,29 +34,16 @@ const eqObjects = function (object1, object2) {
   if (key1.length !== key2.length) {
     return false;
   } else {
-    // Looping through key1 array
     for (const key of key1) {
-      //check if any key is an array
       if (Array.isArray(object1[key])) {
-        // pass the 2 objects to equalArays, return false if eqArray fails
-        if (eqArrays(object1[key], object2[key]) === false) {
-          return false;
-        } else {
-          // if it's true, we will continue to check for another set of array if any
-          continue;
-        }
-        //if none, the program will check primitive values
-      } else {
-        // check the key value pairs matches, if yes, continue with the loop
-        if (object1[key] === object2[key]) {
-          continue;
-        } else {
-          // if it doesn't match, return false
-          return false;
-        }
+        // pass them to eqArrays function
+        return eqArrays(object1[key], object2[key]);
+      }
+      // check if the values match
+      if (object1[key] !== object2[key]) {
+        return false;
       }
     }
-    // if it's pass all the checks without return false, the program will return true
     return true;
   }
 };
@@ -76,8 +63,6 @@ assertEqual(eqObjects(abc, cba), false);
 const cd = { c: "1", d: ["2", 3] };
 const dc = { d: ["2", 3], c: "1" };
 assertEqual(eqObjects(cd, dc), true);
-
-//console.log(eqArrays(dc["d"], cd["d"]));
 
 const cd2 = { c: "1", d: ["2", 3, 4] };
 assertEqual(eqObjects(cd, cd2), true);

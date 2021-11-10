26751d24fef6f2053aecd4d40fd86d3604c7a922
Yen <yen.hnguyen17@gmail.com>
2021-11-10 04:29:51 +0000

    Refactor the existing code

--- eqArrays.js
+++ eqArrays.js
@@ -1,13 +1,5 @@
-// function to check the 2 values if they match
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
-// function to check if the 2 arrays are perfect match
 const eqArrays = (array1, array2) => {
   if (array1.length !== array2.length) {
     return false;
@@ -23,11 +15,7 @@ const eqArrays = (array1, array2) => {
   }
 };
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true)
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true)
-assertEqual(eqArrays(["Yen", "Jackie"], ["Yen"]), false)
-assertEqual(eqArrays(["Yen", "Jackie", "Luna", "Ellie"], ["Yen", "Jackie", "Luna", "Elie"]), true)
-
+module.exports = eqArrays;
 
 
 

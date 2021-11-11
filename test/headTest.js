67021a1dc090878f5e3548770fc7ed804a9d916d
Yen <yen.hnguyen17@gmail.com>
2021-11-11 01:17:27 +0000

    Add mocha and chai

--- test/headTest.js
+++ test/headTest.js
@@ -1,8 +1,11 @@
-const assertEqual = require('../assertEqual');
-const head = require('../head');
+const assert = require('chai').assert;
+const head   = require('../head');
 
-// Test Code
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), undefined);
-assertEqual(head([7]), 7);
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it("returns '5' for ['5']", () => {
+  assert.strictEqual(head(['5']), '5'); 
+  });
+});

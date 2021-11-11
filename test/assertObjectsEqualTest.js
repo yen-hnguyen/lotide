fea1aae1c8fdca8f3bdb35855e1618ff41f8a0e6
Yen <yen.hnguyen17@gmail.com>
2021-11-11 22:43:03 +0000

    Move the Test Code to the new test filei

---
+++ test/assertObjectsEqualTest.js
@@ -0,0 +1,21 @@
+const assertObjectsEqual = require('../assertObjectsEqual');
+
+// Primitives As Values (Test)
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+assertObjectsEqual(ab, ba);
+
+const abc = { a: "1", b: "2", c: "3" };
+assertObjectsEqual(ab, abc);
+
+const cba = { c: "3", a: "1", b: "4" };
+assertObjectsEqual(cba, abc);
+assertObjectsEqual(abc, cba);
+
+// Arrays As Values (Test)
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+assertObjectsEqual(cd, dc);
+
+const cd2 = { c: "1", d: ["2", 3, 4] };
+assertObjectsEqual(cd, cd2);

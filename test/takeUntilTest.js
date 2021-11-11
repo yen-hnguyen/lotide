df8da22d43f79aa8016ac47f356288912b58b3f6
Yen <yen.hnguyen17@gmail.com>
2021-11-11 23:08:54 +0000

    Unit Testing with Mocha & Chai

---
+++ test/takeUntilTest.js
@@ -0,0 +1,15 @@
+const assert = require('chai').assert;
+const takeUntil = require('../takeUntil');
+
+describe('#takeUntil', () => {
+  it('return [1, 2, 5, 7, 2] for data1 &  this callback (x) => x < 0', () => {
+    const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+    const results1 = takeUntil(data1, (x) => x < 0);
+    assert.deepEqual(results1, [1, 2, 5, 7, 2]);
+  });
+  it('return results2, ["I\'ve", "been", "to", "Hollywood"] for data2 & this callback (x) => x === ","', () => {
+    const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+    const results2 = takeUntil(data2, (x) => x === ",");
+    assert.deepEqual(results2, ["I've", "been", "to", "Hollywood"]);
+  });
+});

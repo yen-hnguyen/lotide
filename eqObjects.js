5ac22d1711242916e39fb12937b7897fdef29802
Yen <yen.hnguyen17@gmail.com>
2021-11-11 22:34:16 +0000

    Unit Testing with Mocha & Chai

--- eqObjects.js
+++ eqObjects.js
@@ -1,27 +1,4 @@
-// assertEqual function
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-// eqArray function
-const eqArrays = (array1, array2) => {
-  if (array1.length !== array2.length) {
-    return false;
-  } else {
-    for (let i = 0; i < array1.length; i++) {
-      if (array1[i] !== array2[i]) {
-        return false;
-      } else {
-        continue;
-      }
-    }
-    return true;
-  }
-};
+const eqArrays = require('./eqArrays');
 
 // Function Implementation
 // eqObjects take in two objects and return true of false, base on the perfect match
@@ -48,21 +25,4 @@ const eqObjects = function (object1, object2) {
   }
 };
 
-// Primitives As Values (Test)
-const ab = { a: "1", b: "2" };
-const ba = { b: "2", a: "1" };
-const abc = { a: "1", b: "2", c: "3" };
-const cba = { c: "3", a: "1", b: "4" };
-
-assertEqual(eqObjects(ab, ba), true);
-assertEqual(eqObjects(ab, abc), true);
-assertEqual(eqObjects(cba, abc), true);
-assertEqual(eqObjects(abc, cba), false);
-
-// Arrays As Values (Test)
-const cd = { c: "1", d: ["2", 3] };
-const dc = { d: ["2", 3], c: "1" };
-assertEqual(eqObjects(cd, dc), true);
-
-const cd2 = { c: "1", d: ["2", 3, 4] };
-assertEqual(eqObjects(cd, cd2), true);
+module.exports = eqObjects;
---
+++ test/eqObjectsTest.js
@@ -0,0 +1,25 @@
+const assert = require('chai').assert;
+const eqObjects = require('../eqObjects');
+
+describe('#eqObjects', () => {
+  it('return true when comparing ab vs ba', () => {
+    const ab = { a: "1", b: "2" };
+    const ba = { b: "2", a: "1" };
+    assert.deepEqual(eqObjects(ab, ba), true);
+  });
+  it('return false when comparing ab vs ba', () => {
+    const ab = { a: "1", b: "2" };
+    const abc = { a: "1", b: "2", c: "3" };
+    assert.deepEqual(eqObjects(ab, abc), false);
+  });
+  it('return true when comparing cd vs dc', () => {
+    const cd = { c: "1", d: ["2", 3] };
+    const dc = { d: ["2", 3], c: "1" };
+    assert.deepEqual(eqObjects(cd, dc), true);
+  });
+  it('return false when comparing cs vs cd2', () => {
+    const cd = { c: "1", d: ["2", 3] };
+    const cd2 = { c: "1", d: ["2", 3, 4] };
+    assert.deepEqual(eqObjects(cd, cd2), false);
+  });
+});

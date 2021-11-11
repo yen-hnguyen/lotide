dfd6fb265fc65c7a179927414c951fe84b63461f
Yen <yen.hnguyen17@gmail.com>
2021-11-11 23:14:35 +0000

    Unit Testing with Mocha & Chai

---
+++ test/findKeyTest.js
@@ -0,0 +1,27 @@
+const assert = require('chai').assert;
+const findKey = require('../findKey');
+
+describe('#takeUntil', () => {
+  it('return "Akaleri" when finding first key that has 3 stars ', () => {
+    const object = {
+      "Blue Hill": { stars: 1 },
+      Akaleri: { stars: 3 },
+      noma: { stars: 2 },
+      elBulli: { stars: 3 },
+      Ora: { stars: 2 },
+      Akelarre: { stars: 3 },
+    };
+    assert.deepEqual(findKey(object, (x) => x.stars === 3), "Akaleri");
+  });
+  it('return "noma" when finding first key that has 2 stars ', () => {
+    const object = {
+      "Blue Hill": { stars: 1 },
+      Akaleri: { stars: 3 },
+      noma: { stars: 2 },
+      elBulli: { stars: 3 },
+      Ora: { stars: 2 },
+      Akelarre: { stars: 3 },
+    };
+    assert.deepEqual(findKey(object, (x) => x.stars === 2), "noma");
+  });
+});
